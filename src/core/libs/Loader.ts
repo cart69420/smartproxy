55a9d1e7617feff0da7c9e92e5b490e519bede22
cart1305 <boristheotherslave@gmail.com>
2023-03-11 09:41:42 +0700

    errors on jupyter?

--- src/core/libs/Loader.ts
+++ src/core/libs/Loader.ts
@@ -1,11 +1,10 @@
 import { Client } from 'minecraft-protocol';
-import { Registry } from 'prismarine-registry';
 import { inject as entityInject } from './Entities'
 import { inject as healthInject } from './Health'
 
 declare module 'minecraft-protocol' {
     interface Client {
-        registry: Registry
+        registry: any
     }
 }
 
